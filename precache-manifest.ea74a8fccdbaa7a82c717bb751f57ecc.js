e4f1b3b080aa11e0cde10f21b0d1aa7b56f03897
Anu <anu@gmail.com>
2021-11-21 08:46:43 +0100

    Updates

---
+++ precache-manifest.ea74a8fccdbaa7a82c717bb751f57ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1ce828128a0332a1b9d2ab885f7fdb",
+    "url": "/shoestore/index.html"
+  },
+  {
+    "revision": "b5a6c5b10bc901b0f619",
+    "url": "/shoestore/static/css/main.7b46f031.chunk.css"
+  },
+  {
+    "revision": "b8404299c444261548c4",
+    "url": "/shoestore/static/js/2.e51392bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoestore/static/js/2.e51392bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a6c5b10bc901b0f619",
+    "url": "/shoestore/static/js/main.4e351161.chunk.js"
+  },
+  {
+    "revision": "ba7c660aea650f15ff84",
+    "url": "/shoestore/static/js/runtime-main.d0b37231.js"
+  }
+]);
